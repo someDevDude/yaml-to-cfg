42ca28e61a1857e47b0a801f28bac709fb3c165e
Aaron Moore <aaronm@familyzone.com>
2021-03-10 13:45:42 +0800

    Updating to check path to be more resilient

--- __main__.py
+++ __main__.py
@@ -29,7 +29,7 @@ except IOError:
 blocks = yaml.load(source_file, Loader=yaml.FullLoader)
 for x in blocks:
     # assumption is we only care about things that are dictionary for env variables
-    if x.get('op') == 'add' and type(x['value']) is dict:
+    if x.get('op') == 'add' and x.get('path') == '/spec/template/spec/containers/0/env/-':
         name = x['value']['name']
         value = x['value']['value']
 
